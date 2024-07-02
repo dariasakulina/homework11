6fe954d47b22938c42ee7edda81aa9289e872a33
Daria Sakulina <evon620c@bk.ru>
2024-07-02 22:09:42 +0300

    second

---
+++ src/main/java/FilmManager.java
@@ -0,0 +1,41 @@
+public class FilmManager {
+
+    private String[] titles = new String[0];
+
+    private int limit;
+
+    public FilmManager() {
+        this.limit = 5;
+    }
+
+    public FilmManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addTitle(String title) {
+        String[] tmp = new String[titles.length + 1];
+        for (int i = 0; i < titles.length; i++) {
+            tmp[i] = titles[i];
+        }
+        tmp[tmp.length - 1] = title;
+        titles = tmp;
+    }
+
+    public String[] findAll() {
+        return titles;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (titles.length < limit) {
+            resultLength = titles.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = titles[titles.length - 1 - i];
+        }
+        return tmp;
+    }
+}
---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+    @Test
+    public void shouldFindZeroTitles() {
+        FilmManager manager = new FilmManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddAllTitles() {
+
+        FilmManager manager = new FilmManager();
+
+        manager.addTitle("Бладшот");
+        manager.addTitle("Вперёд");
+        manager.addTitle("Отель Белград");
+        manager.addTitle("Джентльмены");
+        manager.addTitle("Человек-невидимка");
+        manager.addTitle("Тролли. Мировой тур");
+        manager.addTitle("Номер один");
+
+
+        String[] expected = {"Бладшот", "Вперёд", "Отель Белград", "Джентльмены", "Человек-невидимка", "Тролли. Мировой тур", "Номер один"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLast5Titles() {
+
+        FilmManager manager = new FilmManager();
+
+        manager.addTitle("Бладшот");
+        manager.addTitle("Вперёд");
+        manager.addTitle("Отель Белград");
+        manager.addTitle("Джентльмены");
+        manager.addTitle("Человек-невидимка");
+        manager.addTitle("Тролли. Мировой тур");
+        manager.addTitle("Номер один");
+
+        String[] expected = {"Номер один", "Тролли. Мировой тур", "Человек-невидимка", "Джентльмены", "Отель Белград"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindMoreLimitTitles() {
+
+        FilmManager manager = new FilmManager(3);
+
+        manager.addTitle("Бладшот");
+        manager.addTitle("Вперёд");
+        manager.addTitle("Отель Белград");
+        manager.addTitle("Джентльмены");
+        manager.addTitle("Человек-невидимка");
+        manager.addTitle("Тролли. Мировой тур");
+        manager.addTitle("Номер один");
+
+        String[] expected = {"Номер один", "Тролли. Мировой тур", "Человек-невидимка" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
